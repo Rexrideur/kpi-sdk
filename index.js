0945568b5c78f8d32bf458405712989f6969d7d9
Théo Sigaud <sigaudtheo@gmail.com>
2023-07-12 22:49:15 +0200

    Add front sdk

--- index.js
+++ index.js
@@ -1,5 +1,72 @@
+import {useEffect, useRef} from "react";
+import {v4 as uuidv4} from 'uuid';
+
 function helloNpm() {
     return "hello NPM"
 }
 
+function useAnalytics() {
+
+    const userData = useRef(null);
+
+    useEffect(() => {
+        const getUserData = async () => {
+            try {
+                const language = navigator.language;
+                const userAgent = navigator.userAgent;
+                const path = window.location.pathname;
+
+                let id = localStorage.getItem('userId');
+                if (!id) {
+                    id = uuidv4();
+                    if (typeof id === "string") {
+                        localStorage.setItem('userId', id);
+                    }
+                }
+
+                userData.current = {
+                    language: language,
+                    userAgent: userAgent,
+                    id: id,
+                    date: new Date(),
+                    path: path
+                };
+                // navigator.sendBeacon('/analytics', JSON.stringify(userData.current));
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getUserData();
+
+        const handleClick = (event) => {
+            if (userData.current) {
+                const button = (event.target)?.innerText ?? "unknown button";
+                const buttonClickData = {
+                    id: userData.current.id,
+                    button,
+                    date: new Date(),
+                };
+                // navigator.sendBeacon('/analytics', JSON.stringify(buttonClickData));
+            }
+        };
+
+        window.addEventListener("click", handleClick);
+
+        const handleUnload = () => {
+            if (userData.current) {
+                userData.current = null;
+            }
+        };
+
+        window.addEventListener("beforeunload", handleUnload);
+
+        return () => {
+            window.removeEventListener("click", handleClick);
+            window.removeEventListener("beforeunload", handleUnload);
+        };
+    }, []);
+}
+
 module.exports = helloNpm
+module.exports = useAnalytics
