617aa783275b0059ea5d5ed00a005fb67a812f42
Théo Sigaud <sigaudtheo@gmail.com>
2023-07-14 19:16:09 +0200

    test fix heatmap

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-import {useEffect, useRef} from "react";
-import {v4 as uuidv4} from 'uuid';
-import h337 from 'heatmap.js';
-
 export function helloNpm() {
-    let heatmapInstance = h337.create({
-        container: document.querySelector('.heatmap'),
-        radius: 90
-    });
-    document.querySelector('.demo-wrapper').onclick = function (ev) {
-        heatmapInstance.addData({
-            x: ev.layerX,
-            y: ev.layerY,
-            value: 1
-        });
-    };
     return "hello NPM";
 }
 
 export function customHeatMap() {
+    import h337 from 'heatmap.js';
+
     let heatmapInstance = h337.create({
         container: document.querySelector('.heatmap'),
         radius: 90
@@ -34,6 +21,9 @@ export function customHeatMap() {
 }
 
 export function useAnalyticsPage() {
+    import {useEffect, useRef} from "react";
+    import {v4 as uuidv4} from 'uuid';
+
     const userData = useRef(null);
 
     useEffect(() => {
@@ -69,6 +59,9 @@ export function useAnalyticsPage() {
 }
 
 export function useAnalyticsClick() {
+    import {useEffect} from "react";
+    import {v4 as uuidv4} from 'uuid';
+
     useEffect(() => {
         const handleClick = (event) => {
             const target = event.target;
