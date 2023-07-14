100f5e6a6cc2c92994c72608ce116251799b152e
Théo Sigaud <sigaudtheo@gmail.com>
2023-07-14 19:42:22 +0200

    test fix heatmap

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,5 @@ declare module 'kpi-sdk-esgi' {
     export function useAnalyticsClick(): any;
     export function useAnalyticsPage(): void;
     export function helloNpm(): string;
-    export function heatmap(): string;
+    export function heatMap(): string;
 }
