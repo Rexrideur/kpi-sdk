509122f6131ee2fb7b7889a0b00ea1b10a46770b
Théo Sigaud <sigaudtheo@gmail.com>
2023-07-14 18:16:55 +0200

    test fix heatmap

--- index.js
+++ index.js
@@ -74,17 +74,16 @@ export function useAnalyticsClick() {
 
 export function useHeatMap() {
     useEffect(() => {
-        console.log("useHeatMap");
-        // let heatmapInstance = h337.create({
-        //     container: document.querySelector('.heatmap'),
-        //     radius: 90
-        // });
-        // document.querySelector('.demo-wrapper').onclick = function (ev) {
-        //     heatmapInstance.addData({
-        //         x: ev.layerX,
-        //         y: ev.layerY,
-        //         value: 1
-        //     });
-        // };
+        let heatmapInstance = h337.create({
+            container: document.querySelector('.heatmap'),
+            radius: 90
+        });
+        document.querySelector('.demo-wrapper').onclick = function (ev) {
+            heatmapInstance.addData({
+                x: ev.layerX,
+                y: ev.layerY,
+                value: 1
+            });
+        };
     }, []);
 }
