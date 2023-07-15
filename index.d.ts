64c98278fa1008deb9cd468e3e1895831390a787
waseem1104 <waseem11@hotmail.fr>
2023-07-15 21:54:59 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 declare module 'kpi-sdk-esgi' {
-    // export function useAnalyticsClick(): any;
+    export function useAnalyticsClick(): any;
     export function useAnalyticsPage(): any;
     export function helloNpm(): string;
     export function heatMap(): string;
