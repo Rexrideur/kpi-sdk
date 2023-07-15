8d78a57f2d7571690f05ed243f3325ed30d9d0a6
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-07-15 20:16:54 +0200

    Useanalytics page - update index.d

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module 'kpi-sdk-esgi' {
     export function useAnalyticsClick(): any;
-    export function useAnalyticsPage(): void;
+    export function useAnalyticsPage(): any;
     export function helloNpm(): string;
     export function heatMap(): string;
 }
